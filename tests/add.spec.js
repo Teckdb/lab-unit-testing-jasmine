ec0be9a3ff024a1dcc68820af3691f0aaf5b504e
Ruben Diaz <rubendb95@gmail.com>
2024-07-06 19:20:08 +0200

    done lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,24 +1,36 @@
 describe("Iteration 1 | Getting Started", () => {
     describe("Function add", () => {
+      // Test 1
+      // Comprueba si esta la funcion definida en el codigo
       it("should be defined", () => {
         expect(add).toBeDefined();
       });
-  
+      // Test 2
+      // Comprueba si puede coger dos argumentos
       it("should take two arguments", () => {
         expect(add.length).toBe(2);
       });
-  
+      // Test 3
+      // Comprueba si la funcion retonar la suma de dos numeros
       it("should return the sum of the two numbers", () => {
         expect(add(1, 2)).toEqual(3);
         expect(add(3, 4)).toEqual(7);
         expect(add(100, 47)).toEqual(147);
       });
-  
+      // Test 4
+      // Comprueba si la funcion recibe un valor undefined devuelve un valor undefined
       it("should return undefined if any of the arguments is not provided", () => {
         expect(add(1)).toEqual(undefined);
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("The function should return undefined if any of the two arguments is not a number", () => {
+        expect(add(1,"2")).toEqual(undefined);
+        expect(add("1",2)).toEqual(undefined);
+        expect(add("1","2")).toEqual(undefined);
+      });
     });
   });
   
+
+// What do the expect functions do, and what input do they take?
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,36 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
+        it("The function should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
+        it("The function should take two numbers as arguments", () => {
+            expect(calculateArea.length).toBe(2);
+        });
+        it("The function should return a number representing the area of a rectangle (the product of the two arguments)", () => {
+            expect(calculateArea(2,2)).toEqual(4);
+            expect(calculateArea(6,6)).toEqual(36);
+        });
+        it("In case any of the arguments is not provided, the function should return undefined", () => {
+            expect(calculateArea(1)).toEqual(undefined);
+            expect(calculateArea()).toEqual(undefined);
+            expect(calculateArea(undefined,1)).toEqual(undefined);
+            expect(calculateArea(1,undefined)).toEqual(undefined);
+        });
+        it("The function should return undefined if any of the two arguments is not a number", () => {
+            expect(add(1,"2")).toEqual(undefined);
+            expect(add("1",2)).toEqual(undefined);
+            expect(add("1","2")).toEqual(undefined);
+        });
     })    
 })
 
-*/
+// The function should be defined.
+// The function should take two numbers as arguments.
+// The function should return a number representing the area of a rectangle (the product of the two arguments).
+// In case any of the arguments is not provided, the function should return undefined.
